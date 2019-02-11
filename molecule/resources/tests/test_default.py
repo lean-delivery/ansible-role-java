5ab6d6600239454ab3bce41c2b51ccf53d2ce7f4
Timur Gadiev <Timur_Gadiev@epam.com>
2019-02-11 05:40:08 +0300

    Test java 6-7
    Signed-off-by: Timur Gadiev <Timur_Gadiev@epam.com>

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -12,3 +12,8 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_java_version(host):
+    command = host.run('java -version')
+    assert command.rc == 0
